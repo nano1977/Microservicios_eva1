7a495a694beb38fa85d659c6beec8ba5ad1eff97
Manuel Álvarez Guevara <man.alvarezg@duocuc.cl>
2026-03-30 20:45:07 -0300

    Sistema cargando datos de prueba automáticamente

--- demo/src/main/java/com/Logistica/demo/DemoApplication.java
+++ demo/src/main/java/com/Logistica/demo/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.Logistica.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.Logistica.demo.model.Vehiculo;
+import com.Logistica.demo.repository.VehiculoRepository;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +15,17 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(VehiculoRepository repository) {
+		return args -> {
+			Vehiculo v = new Vehiculo();
+			v.setPatente("AB-CD-12");
+			v.setModelo("Volvo FH16 - Camión de Carga");
+			v.setChofer("Juan Pérez");
+			
+			repository.save(v);
+			
+			System.out.println(">>> Vehículo de prueba guardado exitosamente");
+		};
+	}
 }
--- demo/src/main/java/com/Logistica/demo/controller/LogisticaController.java
+++ demo/src/main/java/com/Logistica/demo/controller/LogisticaController.java
@@ -1,13 +1,30 @@
 package com.Logistica.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Logistica.demo.model.Vehiculo;
+import com.Logistica.demo.repository.VehiculoRepository;
+
 @RestController
+@RequestMapping("/api/logistica") // Esto organiza mejor tus rutas
 public class LogisticaController {
+
+    @Autowired
+    private VehiculoRepository vehiculoRepository;
 
     @GetMapping("/saludo")
     public String holaLogistica() {
         return "¡Hola! El microservicio de Logística está funcionando correctamente.";
+    }
+
+    // Nueva ruta para ver los vehículos
+    @GetMapping("/vehiculos")
+    public List<Vehiculo> listarVehiculos() {
+        return vehiculoRepository.findAll();
     }
 }
